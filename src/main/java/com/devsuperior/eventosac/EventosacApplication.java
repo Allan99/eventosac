169098c42ab97631f7816dfb4c77bbd7e8fdd3d7
Allan <allanpereira773@gmail.com>
2026-04-05 16:49:19 -0300

    Creating Activity, Block and Category entities

---
+++ src/main/java/com/devsuperior/eventosac/EventosacApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.eventosac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosacApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/devsuperior/eventosac/entities/Block.java
@@ -0,0 +1,73 @@
+package com.devsuperior.eventosac.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_block")
+public class Block {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant start;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant finish;
+
+    @ManyToOne
+    @JoinColumn(name = "activity_id")
+    private Activity activity;
+
+    public Block(){
+
+    }
+
+    public Block(Long id, Instant start, Instant finish) {
+        this.id = id;
+        this.start = start;
+        this.finish = finish;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getStart() {
+        return start;
+    }
+
+    public void setStart(Instant start) {
+        this.start = start;
+    }
+
+    public Instant getFinish() {
+        return finish;
+    }
+
+    public void setFinish(Instant finish) {
+        this.finish = finish;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Block block = (Block) o;
+        return Objects.equals(id, block.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
+
+
