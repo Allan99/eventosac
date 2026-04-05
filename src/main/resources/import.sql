43dc9cfbb28660b8434a6c965875fce804b88fc9
Allan <allanpereira773@gmail.com>
2026-04-05 18:51:19 -0300

    Seeding tb_activity_participant_table

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,3 +8,14 @@ insert into tb_block (activity_id, start_time, end_time) values (1, '2017-09-25 
 insert into tb_block (activity_id, start_time, end_time) values (2, '2017-09-25 14:00:00', '2017-09-25 18:00:00');
 insert into tb_block (activity_id, start_time, end_time) values (2, '2017-09-26 08:00:00', '2017-09-26 11:00:00');
 
+insert into tb_participant (name, email) values ('José Silva', 'jose@gmail.com');
+insert into tb_participant (name, email) values ('Tiago Faria', 'tiago@gmail.com');
+insert into tb_participant (name, email) values ('Maria do Rosário', 'maria@gmail.com');
+insert into tb_participant (name, email) values ('Teresa Silva', 'teresa@gmail.com');
+
+insert into tb_activity_participant (participant_id, activity_id) values (1, 1);
+insert into tb_activity_participant (participant_id, activity_id) values (1, 2);
+insert into tb_activity_participant (participant_id, activity_id) values (2, 1);
+insert into tb_activity_participant (participant_id, activity_id) values (3, 1);
+insert into tb_activity_participant (participant_id, activity_id) values (3, 2);
+insert into tb_activity_participant (participant_id, activity_id) values (4, 2);
